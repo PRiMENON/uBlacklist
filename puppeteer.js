390f718f93ac14768c30d72721f2d619604eae26
PRiMENON <kuroweb@gmail.com>
2022-11-03 22:30:52 +0900

    add interceptedRequest.

--- puppeteer.js
+++ puppeteer.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 const EvidenceFile = './evidence.md';
 const urlFile = './domain-list.yaml';
-// const urlFile = './test/domain-list.yaml';
 
 function remove_File(paths) {
     paths.forEach(item => {
@@ -44,18 +43,40 @@ function create_evidenceFile(count, data) {
 (async () => {
     const browser = await puppeteer.launch({
         headless: true, // defalut:true, enable debug only 'true';
-        sloMo: 1000, //defalut: comment out, enable debug only.
-        devtools: true, //default: false, enable debug only 'true';
+        // sloMo: 1000, //defalut: comment out, enable debug only.
+        // devtools: true, //default: false, enable debug only 'true';
+        ignoreHTTPSErrors: true,
         defaultViewport: {
             width: 1200,
             height: 900
         },
         timeout: 10000,
+        // https://peter.sh/experiments/chromium-command-line-switches/
         args: [
-            '--incognito'
+            '--incognito', // Enable seacret mode
+            '--disable-gpu', // Disable gpu
+            '--disable-dev-shm-usage', // Change shared memory path /dev/shm to /tmp
+            '--disable-setuid-sandbox', // Disable the setuid sandbox (Linux only)
+            '--no-first-run', // Skip First Run tasks
+            '--no-sandbox', // Disables the sandbox for all process types that are normally sandboxed
+            '--no-zygote', // Disables the use of a zygote process for forking child processes
+            '--single-process', // Runs the renderer and plugins in the same process as the browser 
+            '--ignore-certificate-errors-spki-list', // A set of public key hashes for which to ignore certificate-related errors. 
+            '--ignore-urlfetcher-cert-requests' // Causes net::URLFetchers to ignore requests for SSL client certificates, causing them to attempt an unauthenticated SSL/TLS session.
         ],
     });
     const page = await browser.newPage();
+    await page.setRequestInterception(true);
+    page.on('request', interceptedRequest => {
+        if(interceptedRequest.isInterceptResolutionHandled()) return;
+        if (
+            interceptedRequest.url().endsWith('.png') ||
+            interceptedRequest.url().endsWith('.jpg') ||
+            interceptedRequest.url().endsWith('.js')
+        )
+            interceptedRequest.abort().catch(err => console.error(err));
+        else interceptedRequest.continue();
+    });
     try {
         let removeFiles = [EvidenceFile];
         remove_File(removeFiles);
@@ -65,16 +86,35 @@ function create_evidenceFile(count, data) {
             count++;
             let yaml_domain = yaml['domain'];
             let yaml_evidence = yaml['evidence'];
+            let restext = '';
+
+            let regex = new RegExp(/^http/, 'gi');
+            if (regex.test(yaml_evidence) == false) {
+                console.log('NOTE:' + yaml_evidence + 'is not URL format. skip it.')
+                continue;
+            }
+
             console.log('check => ' + yaml_evidence);
-            const response = await page.goto(yaml_evidence, { waitUntil: 'networkidle2' });
-            console.log(`status: ${response.status()}`);
-            let data = '|' + count + '|`' + yaml_domain + '`|' + yaml_evidence + '|' + response.status() + '|\n';
+
+            const response = await page.goto(yaml_evidence, {
+                waitUntil: 'networkidle2',
+                timeout: 0
+            });
+
+            if (response.status() == 404) {
+                restext = ' Not found';
+            } else if (response.status() == 504) {
+                restext = ' Gateway Timeout';
+            }
+            console.log(`status: ${response.status()}` + restext);
+            let data = '|' + count + '|`' + yaml_domain + '`|' + yaml_evidence + '|' + response.status() + restext + '|\n';
             create_evidenceFile(count, data);
         }
     }
     catch (err) {
         console.error('error=>', err);
     } finally {
+        console.log('script has successfly complete.')
         await browser.close();
     }
 })();
